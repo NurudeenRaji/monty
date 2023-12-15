de01d7e73e7b6c2227f86276c47cd1e54f083b69
Raji Nurudeen <orizon.bot21@gmail.com>
2023-12-15 22:33:26 +0100

    Monty project

--- monty.h
+++ monty.h
@@ -73,6 +73,6 @@ void pop(stack_t **stack, unsigned int line_number);
 void remove_node(void);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
-
+void nop(stack_t **stack, unsigned int line_number);
 
 #endif
